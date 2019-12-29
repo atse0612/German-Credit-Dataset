68e7b37502b1358f5cadbb1315abcdb6c4de74be
A. Tse <atse0612@users.noreply.github.com>
2019-12-28 18:02:50 -0800

    Add files via upload

---
+++ ISYE 6501 Questions 9 and 10.R
@@ -0,0 +1,16 @@
+# ISYE 6501 Questions 9 and 10
+
+## Changing the Working Directory
+
+setwd('./GATech MM Analytics/Introduction to Analytics Modeling/Week 4')
+
+## Loading the Libraries
+
+library(tidyverse)
+library(tree)
+library(rpart)
+library(randomForest)
+library(gtools)
+library(gdata)
+library(pscl)
+library(ROCR)
